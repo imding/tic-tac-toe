3c89519230b2598e25e821ef4cf2ea0137eac724
Siuling Ding <20434337+imding@users.noreply.github.com>
2023-06-15 23:11:17 +0800

    Delete out directory

--- out/_next/static/9bOiz86QHFMtKW3aPYpgF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9c8905e7a678fb42.js"],"/_error":["static/chunks/pages/_error-9af683bd2622dddc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
