f35f8edcc921570c5d3073af1155a0319b533b5a
Siuling Ding <20434337+imding@users.noreply.github.com>
2023-06-15 21:32:39 +0800

    perform e2e tests on main only

--- out/cypress/e2e/game.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('tic-tac-toe game', () => {
-    beforeEach(() => {
-        cy.visit('https://imding.github.io/tic-tac-toe')
-    })
-
-    it('passes', () => {
-        cy.get('h1.text-white').should('have.length', 1)
-    })
-})
